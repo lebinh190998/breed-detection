6e53da6d5baacb06b61dc9ec20ed97dec891cf94
Binh Le <lebinh190998@gmail.com>
2023-07-16 23:04:51 +0700

    can u just work

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-
-if [ "$RUN_ENV" = "production" ]; then
-    pip install torch==1.13.1+cpu torchvision==0.14.1+cpu --extra-index-url https://download.pytorch.org/whl/cpu
-fi
-
 poetry lock
 poetry config virtualenvs.create false
 
-poetry install --no-interaction --no-ansi
+if [ "$RUN_ENV" = "production" ]; then
+    poetry install --no-interaction --no-ansi --config pyproject.prod.toml
+else
+    poetry install --no-interaction --no-ansi --config pyproject.dev.toml
+fi
+
 poetry add python-multipart --no-interaction --no-ansi
