9dcb8dea25a66e4544ab035f7a37f0debc3d1c9f
Binh Le <lebinh190998@gmail.com>
2023-07-16 22:38:47 +0700

    change order

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-poetry lock
-poetry config virtualenvs.create false
 
 if [ "$RUN_ENV" = "production" ]; then
   python3 -m pip install --no-cache-dir torch==1.13.1+cpu -f "https://download.pytorch.org/whl/cpu/torch_stable.html"
   python3 -m pip install --no-cache-dir torchvision==0.14.1+cpu -f "https://download.pytorch.org/whl/cpu/torch_stable.html"
 fi
+
+poetry lock
+poetry config virtualenvs.create false
 
 poetry install --no-interaction --no-ansi
 poetry add python-multipart --no-interaction --no-ansi
