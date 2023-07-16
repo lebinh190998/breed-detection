8875eb16b3a01fbd72a881b1f8e089df40f056cf
Binh Le <lebinh190998@gmail.com>
2023-07-16 10:43:13 +0700

    add try catch

--- breed_detection/routers/detect.py
+++ breed_detection/routers/detect.py
@@ -11,8 +11,15 @@ router = APIRouter(
 
 @router.post("/")
 async def detect_breed(image: UploadFile):
-    breed = await predict(image)
-    return JSONResponse(content={"breed": breed})
+    try:
+        breed = await predict(image)
+        return JSONResponse(content={"breed": breed})
+    
+    except FileNotFoundError:
+        return JSONResponse(content={"error": "Pkl file not found"}, status_code=404)
+    
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 def init_app(app: FastAPI):
     app.include_router(router)
