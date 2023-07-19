80d4be8e215b2ac2cbf9c55e29e7822f918632fa
Binh Le <lebinh190998@gmail.com>
2023-07-19 20:50:14 +0700

    set torch cpu

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ poetry lock
 poetry config virtualenvs.create false
 
 if [ "$RUN_ENV" = "production" ]; then
-    # poetry add https://download.pytorch.org/whl/cpu/torch-1.13.1%2Bcpu-cp39-cp39-linux_x86_64.whl
-    # poetry add https://download.pytorch.org/whl/cpu/torchvision-0.14.1%2Bcpu-cp39-cp39-linux_x86_64.whl
+    poetry add https://download.pytorch.org/whl/cpu/torch-1.13.1%2Bcpu-cp39-cp39-linux_x86_64.whl
+    poetry add https://download.pytorch.org/whl/cpu/torchvision-0.14.1%2Bcpu-cp39-cp39-linux_x86_64.whl
 
     poetry install --no-interaction --no-ansi --no-dev
 else
