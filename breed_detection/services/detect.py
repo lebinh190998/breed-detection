8a482d331deeeee943d947c0332d4b6fdf3f25fe
Binh Le <lebinh190998@gmail.com>
2023-07-21 01:08:30 +0700

    try again

--- breed_detection/services/detect.py
+++ breed_detection/services/detect.py
@@ -1,10 +1,17 @@
 from fastai.vision.all import *
 import os
 
+def label_breed(x): 
+    filename = x.split('/')[-1]
+    label = ' '.join(word.capitalize() for word in filename.split('_')[:-1])
+    return label
+
+
 # Construct the absolute file path
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 experiment_directory = os.path.join(base_path, 'experiment')
 pkl_file_path = os.path.join(experiment_directory, 'breed_identifier_model.pkl')
+
 print("---- LOADING LEANER ----")
 learn = load_learner(pkl_file_path, cpu=True)
 
--- scripts/entry_point.sh
+++ scripts/entry_point.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-# gunicorn translation.asgi:app -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker --reload
-python run.py
+gunicorn breed_detection.asgi:app -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker --reload
+# python run.py
