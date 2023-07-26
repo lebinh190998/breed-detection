b5e298305fd6f564944748d5a4092c7c1231046f
Binh Le <lebinh190998@gmail.com>
2023-07-26 21:52:32 +0700

    dont know if this work, worth a try

--- breed_detection/services/detect.py
+++ breed_detection/services/detect.py
@@ -1,20 +1,14 @@
 from fastai.vision.all import *
 import os
 
-def label_breed(x): 
-    filename = x.split('/')[-1]
-    label = ' '.join(word.capitalize() for word in filename.split('_')[:-1])
-    return label
-
-
 # Construct the absolute file path
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 pkl_file_path = os.path.join(base_path, 'breed_identifier_model.pkl')
 
-print("---- LOADING LEANER ----")
-learn = load_learner(pkl_file_path, cpu=True)
-
 async def predict(image) -> str:
+    print("---- LOADING LEANER ----")
+    learn = load_learner(pkl_file_path, cpu=True)
+    
     # Load the .pkl file
     print("---- OPENING IMAGE ----")
     im = Image.open(image.file)
--- run.py
+++ run.py
@@ -8,10 +8,28 @@ import os
 import sys
 import uvicorn
 import yaml
-
+from fastai.vision.all import *
 from breed_detection.asgi import app
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+path = untar_data(URLs.PETS)/'images'
+
+def label_breed(x): 
+    filename = x.split('/')[-1]  # Extract the file name from the path if applicable
+    label = ' '.join(word.capitalize() for word in filename.split('_')[:-1])  # Remove the last part (number) after the last underscore
+    return label
+
+dls = ImageDataLoaders.from_name_func('.',
+    get_image_files(path), valid_pct=0.2, seed=42,
+    label_func=label_breed,
+    item_tfms=Resize(192))
+
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+# learn.fine_tune(4)
+
+MODEL_PATH = os.path.join(CURRENT_DIR, 'breed_detection/breed_identifier_model.pkl')
+learn.export(MODEL_PATH)
 
 if __name__ == "__main__":
     if sys.argv[-1] == "openapi":
